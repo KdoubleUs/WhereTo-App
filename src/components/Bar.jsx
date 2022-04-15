bafe92abbb9a2d0e507c4b01c459e30e75c4b4b5
Ainsley Brundage <abrundage95@gmail.com>
2022-04-15 15:43:43 -0400

    Built out bar component

---
+++ src/components/Bar.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+export default function Bar({ bar }) {
+  return (
+    <div>
+      <Link to={`/bars/${bar._id}`}>
+        <h2>{bar.name}</h2>
+      </Link>
+    </div>
+  );
+}
