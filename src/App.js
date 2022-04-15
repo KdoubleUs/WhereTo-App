9d3a361d608cc93e9b10aa8437b71faa74abe5e8
KW <88893976+KdoubleUs@users.noreply.github.com>
2022-04-15 11:31:55 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
+import styled from "styled-components";
 import Navigation from "./components/navigation";
 import Title from "./components/Title";
 import { Link, Routes, Route } from "react-router-dom";
+import Submit from "./components/Submit";
+import Contact from "./components/Contact";
+import RestaurantCreate from "./screen/RestaurantCreate";
 function App() {
   return (
     <div className="app">
       <Navigation />
-      <div>
-        <Title />
-      </div>
+      <Routes>
+        <Route path="/" element={<Title />} />
+        <Route path="/submit" element={<Contact />} />
+        <Route path="/spots/create" element={<RestaurantCreate />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/services/apiConfig.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+let apiUrl;
+const mongoDBCloudUrl =
+  "https://restaurant-backend-appication.herokuapp.com/api";
+const local = "http://localhost:3000/";
+const apiUrls = {
+  production: mongoDBCloudUrl,
+  development: local,
+};
+if (window.location.hostname === "localhost") {
+  apiUrl = apiUrls.development;
+} else {
+  apiUrl = apiUrls.production;
+}
+
+const api = axios.create({
+  baseURL: apiUrl,
+});
+
+export default api;
