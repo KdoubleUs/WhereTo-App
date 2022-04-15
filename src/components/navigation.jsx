406a91e0e731ffaa9c1ee4e982e4e70966a0a1c2
Ainsley Brundage <abrundage95@gmail.com>
2022-04-15 18:04:10 -0400

    Fixed grammatical errors

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
-import { Navbar } from "react-bootstrap";
-import { NavDropdown } from "react-bootstrap";
-import { Nav } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import style from "../csscomponents/navigation.css";
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { Navbar } from 'react-bootstrap';
+import { NavDropdown } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import style from '../csscomponents/navigation.css';
 function Navigation() {
   return (
     <Navbar bg="dark" expand="lg" className="navigation">
       <Container>
         <Navbar.Brand href="/" text="white">
-          NY SPOTS
+          whereTo
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -18,15 +18,15 @@ function Navigation() {
             <Nav.Link href="/">Home</Nav.Link>
             <NavDropdown title="Restaurant" id="basic-nav-dropdown">
               <NavDropdown.Item href="/restaurants">
-                Find A Restaurant
+                Find a restaurant
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Find A Bar</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Find a bar</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">
-                Find An Acitivity
+                Find an activity
               </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="/spots/create">
-                Submit A Spot
+                Submit a spot
               </NavDropdown.Item>
             </NavDropdown>
 
