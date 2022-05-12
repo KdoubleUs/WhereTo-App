31ec57c2bfbf012e7f0710a2b0a550deafa71c4b
KW <88893976+KdoubleUs@users.noreply.github.com>
2022-05-11 22:11:45 -0400

    added loader

--- src/screens/Restaurant/Restaurants.jsx
+++ src/screens/Restaurant/Restaurants.jsx
@@ -1,27 +1,35 @@
-import { useState, useEffect } from 'react';
-import { getRestaurant, getRestaurants } from '../../services/restaurants';
-import Restaurant from '../../components/Restaurant';
-import Container from '../../components/Container';
+import { useState, useEffect } from "react";
+import { getRestaurant, getRestaurants } from "../../services/restaurants";
+import Restaurant from "../../components/Restaurant";
+import Container from "../../components/Container";
+import gif from "../../Dual Ball-1s-200px.gif";
 function Restaurants() {
   const [restaurants, setRestaurants] = useState([]);
+  const [loaded, setLoaded] = useState(null);
   useEffect(() => {
     const fetchRestaurant = async () => {
       const allRestaurant = await getRestaurants();
       setRestaurants(allRestaurant);
+      setLoaded("complete");
+      // setLoaded("complete");z
     };
-    fetchRestaurant();
+    setInterval(() => {
+      fetchRestaurant();
+    }, 2000);
   }, []);
-
+  let loadingGif = <img src={gif} alt="none" />;
   return (
     <div className="restaurants">
       <h1 className="title">Top Choices for Restaurants in NYC</h1>
-      {restaurants.map((restaurant) => (
-        <div>
-          <span>
-            <Container restaurant={restaurant} />
-          </span>
-        </div>
-      ))}
+      {!loaded
+        ? loadingGif
+        : restaurants.map(restaurant => (
+            <div key={restaurant.id}>
+              <span>
+                <Container restaurant={restaurant} />
+              </span>
+            </div>
+          ))}
     </div>
   );
 }
